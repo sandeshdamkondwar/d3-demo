afeb5abad342a8c128260a77b54de71b5fd251ed
Sandesh <sandeshdamkondwar@gmail.com>
2014-12-16 02:28:07 +0530

    Added dynamic colors

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
-var barData = [10, 20, 30, 40, 10, 20, 30, 40, 50,10, 20, 30, 40, 10, 20, 30, 40, 50,10, 20, 30, 40, 10, 20, 30, 40, 50];
+var barData = [];
 
+for (var i = 1000; i >= 0; i--) {
+	barData.push(Math.random() * 30);
+}
+console.profile('perfCheck');
 var height =400,
 	width = 600;
 
@@ -11,6 +15,10 @@ var xScale = d3.scale.ordinal()
 	.domain(d3.range(barData.length))
 	.rangeBands([0, width]);
 
+var colors = d3.scale.linear()
+			   .domain([0, barData.length * 0.33, barData.length * 0.66, barData.length])
+			   .range(["#b58929", "#c61c6f", "#268bd2", "#85992c"]);
+
 d3.select("#chart").append('svg')
 	.attr({
 		width: width,
@@ -19,7 +27,9 @@ d3.select("#chart").append('svg')
 	.style({background: '#c9d7d6'})
 	.selectAll('rect').data(barData)
 	.enter().append('rect')
-		.style({fill: "#C61C6f"})
+		.style({fill: function (data, index) {
+			return colors(index);
+		}})
 		.attr({
 			width: xScale.rangeBand(),
 			height: function (data) {
@@ -32,3 +42,4 @@ d3.select("#chart").append('svg')
 				return height - yScale(data);
 			}
 		});
+console.profileEnd('perfCheck');
